5ec2abc10ea51ea072a594c2dd0603d94eb7e873
m.traboulsi@brgm.fr <m.traboulsi@brgm.fr>
2022-04-08 15:06:54 +0200

    indentation

--- ST_INITIALIZATION.F90
+++ ST_INITIALIZATION.F90
@@ -216,21 +216,21 @@ module st_initialization
     end subroutine read_xshore
 
     subroutine check_errors
-        ! check for switches (should be 0 or 1)
-            call assert(rock .eq. 0 .or. rock .eq. 1, &
-                        "rock must be 0 or 1", 0) ! rock switch
-            call assert((slump%switch .eq. 0) .or. (slump%switch .eq. 1), &
-                'slump must be 0 or 1', 0) ! slump switch
+    ! check for switches (should be 0 or 1)
+        call assert(rock .eq. 0 .or. rock .eq. 1, &
+                    "rock must be 0 or 1", 0) ! rock switch
+        call assert((slump%switch .eq. 0) .or. (slump%switch .eq. 1), &
+            'slump must be 0 or 1', 0) ! slump switch
 
-            ! slumping parameters
-            if (slump%switch .eq. 1) then
-                ! check if slope is between -180 and 180
-                call assert(slump%slope.ge.-180 .and. slump%slope .le. 180,&
-                        'dune slope must be between -180 and 180', 0)
-                ! check if slump cap is positive
-                call assert(slump%cap.ge.0,'slump cap set to a negative' //&
-                                            ' value') !only warning
-            end if
+        ! slumping parameters
+        if (slump%switch .eq. 1) then
+            ! check if slope is between -180 and 180
+            call assert(slump%slope.ge.-180 .and. slump%slope .le. 180,&
+                    'dune slope must be between -180 and 180', 0)
+            ! check if slump cap is positive
+            call assert(slump%cap.ge.0,'slump cap set to a negative' //&
+                                        ' value') !only warning
+        end if
 
         end subroutine check_errors
 
