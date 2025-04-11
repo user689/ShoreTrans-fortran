7c1bc4e251beab6e421ae0ea7868f60a32a8ce95
Mohammad <m.traboulsi@brgm.fr>
2025-04-11 13:14:41 +0200

    remove check for allocated array

--- src/ST_HELPER.F90
+++ src/ST_HELPER.F90
@@ -121,7 +121,7 @@ contains
 
       out_name_tmp = adj(dir_name) &
          // '/outputs/' // adj(out_name)
-      if (present(arr2_).and. allocated(arr2)) then
+      if (present(arr2_)) then
          allocate(arr2(size(arr2_)))
          arr2 = arr2_
       end if
@@ -137,7 +137,7 @@ contains
          stop
       end if
       ! write the array
-      if (present(arr2_) .and. allocated(arr2)) then
+      if (present(arr2_)) then
          do i = 1, size(arr,1)
             write(fid_write, '(F0.4, A, F0.4)') arr(i), ' ', arr2(i)
          end do
